6e900533e2b79f0e00c514d1235aa96735428523
dxyyyyyy <1315685423@qq.com>
2026-06-08 09:38:13 +0800

    Add inventory & collectable objects
    Introduce an inventory system and collectables: add InventoryManager (autoload) and new scripts/ui (inventory_panel) and scripts/globals/inventory_manager.gd, and wire collectable_component to call InventoryManager.add_collectable on pickup. Add several collectable scenes (egg, milk, corn_harvest, tomato_harvest) and a test scene for collectables/inventory management. Tweak object scenes (stone, log) to adjust collision radii/ordering and update test player scene references. Also import a new ZX Palm font (font file, import settings and variation) and move tool_manager.gd into scripts/globals.

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -8,5 +8,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		InventoryManager.add_collectable(collectable_name)
 		print("Collected: " + collectable_name)
 		get_parent().queue_free() # Remove the collectable from the scene
