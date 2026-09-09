157969c5636b6f9b3c648de0f01b35a4a1e20200
dxyyyyyy <1315685423@qq.com>
2026-09-09 11:05:06 +0800

    Add field/crops cursor & tilled dirt tiles
    Introduce field and crops cursor components and support for tilled dirt terrain. Added FieldCursorComponent and CropsCursorComponent scripts and scenes, plus a test scene (test_scene_crops_fields_tilled_dirt.tscn). Updated game_tile_set.tres to include a new tilled dirt atlas texture and terrain entries. Added input actions (remove_dirt, release_tool) to project.godot and placed the Player node in the "player" group so cursor components can reference it.

---
+++ scenes/components/crops_cursor_component.gd
@@ -0,0 +1,27 @@
+class_name CropsCursorComponent
+extends Node
+
+@export var tilled_soil_tilemap_layer: TileMapLayer
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var corn_plant_scene = preload("res://scenes/objects/plants/corn.tscn")
+var tomato_plant_scene = preload("res://scenes/objects/plants/tomato.tscn")
+ 
+var mouse_position: Vector2
+var cell_position: Vector2i
+var cell_source_id: int
+var local_cell_position: Vector2
+var distance: float
+
+func _unhandled_input(event: InputEvent) -> void:
+	get_cell_under_mouse()
+
+			
+func get_cell_under_mouse() -> void:
+	mouse_position = tilled_soil_tilemap_layer.get_local_mouse_position()
+	cell_position = tilled_soil_tilemap_layer.local_to_map(mouse_position)
+	cell_source_id = tilled_soil_tilemap_layer.get_cell_source_id(cell_position)
+	local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
+	distance = player.global_position.distance_to(local_cell_position)
+	
